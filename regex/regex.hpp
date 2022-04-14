9f471f2d1a2ffb1d98c1b6f818a177de8e0a5e20
Charles Helmich <charlie.helmich@gmail.com>
2022-04-14 08:42:00 -0600

    Document Regex match and partial_match functions

--- regex/regex.hpp
+++ regex/regex.hpp
@@ -23,7 +23,12 @@ public:
     RE::Expr const* expr() const { return m_expr; }
     DFA<char> const& dfa() const { return m_dfa; }
 
+    /// @brief Check if this regular expression matches a given string
+    /// @returns @c true if the entire string matches, @c false otherwise
     bool match(std::string_view input) const;
+
+    /// @brief Find the longest match starting at the begininning of a given string
+    /// @returns The number of characters matched if one exists, @c std::nullopt otherwise
     std::optional<size_t> match_partial(std::string_view input) const;
 
 private:
